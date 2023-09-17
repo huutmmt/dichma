95f5a474507da6f086b3b260aa5c5649c7f2ac97
BuildTools <unconfigured@null.spigotmc.org>
2023-09-17 23:49:48 +0700

    huu

--- LogiFace/detector.py
+++ LogiFace/detector.py
@@ -4,7 +4,7 @@ from PIL import Image
 import pickle
 import sqlite3
 
-# thay đổi
+# thay đổi gititdddddd
 
 faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cam=cv2.VideoCapture(0)
